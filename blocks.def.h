c04fd218154689c71fbf6584f7bcb4d43b22c713
Najib Mouqtadi <najib@mouqtadi.com>
2022-01-09 22:22:39 +0100

    weather, volume, battery and clock modules

--- blocks.def.h
+++ blocks.def.h
@@ -1,9 +1,11 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+	// {"", "sb-price btc Bitcoin   ", 9000, 21},
+	{"", "sb-forecast", 18000, 5},
+	{"", "sb-volume", 0, 10},
+	{"", "sb-battery", 5, 3},
+	{"", "sb-clock", 60, 1},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
